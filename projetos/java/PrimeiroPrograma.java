dda6d28abe67fbbea440241fd18f042bbe4ebc8b
renatosilvaunicep <rfranca@unicep.com.br>
2022-03-28 21:26:29 -0300

    atualizacao do primeiroprograma java utilizando eclipse ide

--- projetos/java/PrimeiroPrograma.java
+++
@@ -1,7 +0,0 @@
-public class PrimeiroPrograma {
-	public static void main(String args[]){
-		int a = 4;
-		int b = 6;
-		System.out.println("Hello World!" + (a + b));
-	}
-}
---
+++ projetos/java/primeiro-programa-java/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 15;
+		System.out.println("Hello World!, " + (a + b));
+	}
+
+}
